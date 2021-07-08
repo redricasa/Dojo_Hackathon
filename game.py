2cc7f81e0e43eed40cf7393157026755433d23e0
Frederica Blissett <redricasa@users.noreply.github.com>
2021-07-08 16:19:00 -0700

    updated based on Jin's and Cody's comments

--- game.py
+++ game.py
@@ -3,24 +3,25 @@ from classes.pirate import Pirate
 
 michelangelo = Ninja("Michelanglo", 10,5,100)
 jack_sparrow = Pirate("Jack Sparrow", 15,3,100)
+Captain_Hook = Pirate("cpt Hook", 10,5,90)
 
 # function to loop thru both characters until self.health <= 0
-def see_who_dies_first(self ):
+def see_who_dies_first(Pirate, Ninja, Pirate2):
     # for loop to loop thru until one of them have self.health == 0
-    for i in range(self.health >=0):
-        michelangelo.attack(jack_sparrow)
-        jack_sparrow.show_stats()
-        # use die method
+    # TODO if/else to ensure Michelangelo doesn't 
+    Ninja.attack(Pirate)
+    Pirate.show_stats()
+    # use die method
 
-        jack_sparrow.attack(michelangelo)
-        michelangelo.show_stats()
-    # use die method?
-    return self
+    Pirate.attack(Ninja)
+    Ninja.show_stats()
+
+    Pirate2.attack(Ninja)
+    Ninja.show_stats()
 # while loop with ninja and pirate atacking each other
-def run_fight(self ):
-    i = self.Pirate, self.Ninja
-    while i >= 0:
-        return see_who_dies_first()
-    # return self
+def run_fight( Pirate, Ninja, Pirate2):
+    
+    while Pirate.health >= 0 and Ninja.health >=0 and Pirate2.health >= 0:
+        see_who_dies_first(Pirate, Ninja , Pirate2)
 
-# self.Pirate.health >= 0 and self.Ninja.health >= 0
+run_fight(jack_sparrow, michelangelo, Captain_Hook)
